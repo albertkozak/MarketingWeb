142788fb5bb91f5ea693ef15cd132d35dd17c8ee
crystal mclean <cmclean48@my.bcit.ca>
2020-04-24 22:15:46 -0700

    merge app.js

--- atack-marketing/src/App.js
+++ atack-marketing/src/App.js
@@ -1,42 +1,38 @@
-import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import "./login.css";
-=======
->>>>>>> master
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./screens/Home";
-import Navbar from "./components/Navbar";
-import Events from "./screens/Events";
-import Register from "./screens/Register";
-import Login from "./screens/Login";
-import "./App.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './screens/Home';
+import Navbar from './components/Navbar';
+import Events from './screens/Events';
+import Register from './screens/Register';
+import Login from './screens/Login';
+import './App.css';
+import './login.css';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/Home">
-          <Home />
-        </Route>
-        <Route path="/Events">
-          <Events />
-        </Route>
-        <Route path="/Login">
-          <Login />
-        </Route>
-        <Route path="/Register">
-          <Register />
-        </Route>
-      </Router>
-      {/* <Footer /> */}
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Navbar />
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/Home">
+					<Home />
+				</Route>
+				<Route path="/Events">
+					<Events />
+				</Route>
+				<Route path="/Login">
+					<Login />
+				</Route>
+				<Route path="/Register">
+					<Register />
+				</Route>
+			</Router>
+			{/* <Footer /> */}
+		</div>
+	);
 }
 
 export default App;
