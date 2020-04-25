42210140336a75317b6737638a3780fbaff26159
Albert Kozak <albert.kozak@live.com>
2020-04-25 14:27:14 -0700

    Applied formatting to exisiting files

--- atack-marketing/src/screens/authentication/Login.js
+++ atack-marketing/src/screens/authentication/Login.js
@@ -4,7 +4,7 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import firebase from '../../firebase';
 import { useHistory } from 'react-router-dom';
-import Logo from "../../assets/full-logo.png"
+import Logo from '../../assets/full-logo.png';
 
 const Login = () => {
 	//const API_CREATE_URL =
@@ -17,15 +17,9 @@ const Login = () => {
 				await setTimeout(() => {
 					firebase
 						.auth()
-						.signInWithEmailAndPassword(
-							values.email,
-							values.password
-						)
+						.signInWithEmailAndPassword(values.email, values.password)
 						.then(() => {
-							if (
-								firebase.auth().currentUser.emailVerified ===
-								false
-							) {
+							if (firebase.auth().currentUser.emailVerified === false) {
 								reject(
 									'please verify your email address from the verification email sent to your inbox'
 								);
@@ -33,14 +27,13 @@ const Login = () => {
 							firebase
 								.auth()
 								.currentUser.getIdTokenResult()
-
 								.then((tokenResponse) => {
 									console.log(tokenResponse);
 									fetch(API_GET_URL, {
 										method: 'GET',
 										headers: {
-											Authorization: `Bearer ${tokenResponse.token}`,
-										},
+											Authorization: `Bearer ${tokenResponse.token}`
+										}
 										//	})
 										// fetch(API_CREATE_URL, {
 										// 	method: 'POST',
@@ -51,10 +44,7 @@ const Login = () => {
 										if (response.status === 200) {
 											resolve(response.status);
 										} else {
-											reject(
-												'API ERROR: ' +
-												JSON.stringify(response)
-											);
+											reject('API ERROR: ' + JSON.stringify(response));
 										}
 									});
 
@@ -70,97 +60,70 @@ const Login = () => {
 
 	return (
 		<div className="login-container">
-			<img className="logo" src={Logo} alt="logo"/>
-		<div className="wrapper">
-			<h1>Login</h1>
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				onSubmit={async (values, { setSubmitting, resetForm }) => {
-					try {
-						await handleSubmit(values);
-						resetForm();
-						history.push('/Home');
-					} catch (error) {
-						alert(error);
-						values.password = '';
-					}
-					//	setTimeout(() => {
-					//		console.log('Logging in', values);
-					//		setSubmitting(false);
-					//	}, 1000);
-				}}
-				validationSchema={Yup.object().shape({
-					email: Yup.string().email().required('Required'),
-					password: Yup.string()
-						.required('No password provided.')
-						.min(
-							6,
-							'Password is too short - should be 6 chars minimum.'
-						)
-						.matches(
-							/(?=.*[0-9])/,
-							'Password must contain a number.'
-						),
-				})}
-			>
-				{(props) => {
-					const {
-						values,
-						touched,
-						errors,
-						isSubmitting,
-						handleChange,
-						handleBlur,
-						handleSubmit,
-					} = props;
+			<img className="logo" src={Logo} alt="logo" />
+			<div className="wrapper">
+				<h1>Login</h1>
+				<Formik
+					initialValues={{ email: '', password: '' }}
+					onSubmit={async (values, { setSubmitting, resetForm }) => {
+						try {
+							await handleSubmit(values);
+							resetForm();
+							history.push('/Home');
+						} catch (error) {
+							alert(error);
+							values.password = '';
+						}
+						//	setTimeout(() => {
+						//		console.log('Logging in', values);
+						//		setSubmitting(false);
+						//	}, 1000);
+					}}
+					validationSchema={Yup.object().shape({
+						email: Yup.string().email().required('Required'),
+						password: Yup.string()
+							.required('No password provided.')
+							.min(6, 'Password is too short - should be 6 chars minimum.')
+							.matches(/(?=.*[0-9])/, 'Password must contain a number.')
+					})}
+				>
+					{(props) => {
+						const { values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit } = props;
 
-					return (
-						<form onSubmit={handleSubmit}>
-							<label htmlFor="email">Email</label>
-							<input
-								name="email"
-								type="text"
-								placeholder="Enter your email"
-								value={values.email}
-								onChange={handleChange}
-								onBlur={handleBlur}
-								className={
-									errors.email && touched.email && 'error'
-								}
-							/>
-							{errors.email && touched.email && (
-								<div className="input-feedback">
-									{errors.email}
-								</div>
-							)}
+						return (
+							<form onSubmit={handleSubmit}>
+								<label htmlFor="email">Email</label>
+								<input
+									name="email"
+									type="text"
+									placeholder="Enter your email"
+									value={values.email}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.email && touched.email && 'error'}
+								/>
+								{errors.email && touched.email && <div className="input-feedback">{errors.email}</div>}
 
-							<label htmlFor="email">Password</label>
-							<input
-								name="password"
-								type="password"
-								placeholder="Enter your password"
-								value={values.password}
-								onChange={handleChange}
-								onBlur={handleBlur}
-								className={
-									errors.password &&
-									touched.password &&
-									'error'
-								}
-							/>
-							{errors.password && touched.password && (
-								<div className="input-feedback">
-									{errors.password}
-								</div>
-							)}
-							<button type="submit" disabled={isSubmitting}>
-								Login
-							</button>
-						</form>
-					);
-				}}
-			</Formik>
-		</div>
+								<label htmlFor="email">Password</label>
+								<input
+									name="password"
+									type="password"
+									placeholder="Enter your password"
+									value={values.password}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.password && touched.password && 'error'}
+								/>
+								{errors.password &&
+								touched.password && <div className="input-feedback">{errors.password}</div>}
+								<button type="submit" disabled={isSubmitting}>
+									Login
+								</button>
+							</form>
+						);
+					}}
+				</Formik>
+			</div>
 		</div>
 	);
 };
--- atack-marketing/src/screens/authentication/Register.js
+++ atack-marketing/src/screens/authentication/Register.js
@@ -4,11 +4,10 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import firebase from '../../firebase';
 import { useHistory } from 'react-router-dom';
-import Logo from "../../assets/full-logo.png"
+import Logo from '../../assets/full-logo.png';
 
 const Register = () => {
-	const API_CREATE_URL =
-		'https://atackmarketingapi.azurewebsites.net/api/User/create';
+	const API_CREATE_URL = 'https://atackmarketingapi.azurewebsites.net/api/User/create';
 	const history = useHistory();
 	async function handleSubmit(values) {
 		return new Promise(async (resolve, reject) => {
@@ -17,43 +16,27 @@ const Register = () => {
 					console.log('AAAA');
 					firebase
 						.auth()
-						.createUserWithEmailAndPassword(
-							values.email,
-							values.password
-						)
-
+						.createUserWithEmailAndPassword(values.email, values.password)
 						.then((response) => {
 							console.log(response);
-							alert(
-								' - ' +
-								response.user.email +
-								' Please check verification email'
-							);
+							alert(' - ' + response.user.email + ' Please check verification email');
 
 							firebase.auth().currentUser.sendEmailVerification();
 
-							firebase
-								.auth()
-								.currentUser.getIdTokenResult()
-								.then((tokenResponse) => {
-									fetch(API_CREATE_URL, {
-										method: 'POST',
-										headers: {
-											Authorization: `Bearer ${tokenResponse.token}`,
-										},
-									}).then((response) => {
-										if (response.status === 201) {
-											resolve(response.status);
-										} else {
-											reject(
-												console.log(
-													'API ERROR: ' +
-													JSON.stringify(response)
-												)
-											);
-										}
-									});
+							firebase.auth().currentUser.getIdTokenResult().then((tokenResponse) => {
+								fetch(API_CREATE_URL, {
+									method: 'POST',
+									headers: {
+										Authorization: `Bearer ${tokenResponse.token}`
+									}
+								}).then((response) => {
+									if (response.status === 201) {
+										resolve(response.status);
+									} else {
+										reject(console.log('API ERROR: ' + JSON.stringify(response)));
+									}
 								});
+							});
 						})
 						.catch((e) => {
 							console.log(e);
@@ -65,127 +48,89 @@ const Register = () => {
 
 	return (
 		<div className="login-container">
-		<img className="logo" src={Logo} alt="logo"/>
-		<div className="wrapper">
-			<h1>Register</h1>
-			<Formik
-				initialValues={{ email: '', password: '', confirmPassword: '' }}
-				onSubmit={async (values, { setSubmitting, resetForm }) => {
-					try {
-						await handleSubmit(values);
-						history.push('/Login');
-					} catch (error) {
-						alert(error);
-						resetForm();
-						values.password = '';
-					}
-					//	setTimeout(() => {
-					//		console.log('Logging in', values);
-					//		setSubmitting(false);
-					//	}, 1000);
-				}}
-				validationSchema={Yup.object().shape({
-					email: Yup.string().email().required('Required'),
-					password: Yup.string()
-						.required('No password provided.')
-						.min(
-							6,
-							'Password is too short - should be 6 chars minimum.'
-						)
-						.matches(
-							/(?=.*[0-9])/,
-							'Password must contain a number.'
-						),
-					confirmPassword: Yup.string()
-						.oneOf(
-							[Yup.ref('password')],
-							'Confirm Password must matched Password'
-						)
-						.required('Confirm Password is required'),
-				})}
-			>
-				{(props) => {
-					const {
-						values,
-						touched,
-						errors,
-						isSubmitting,
-						handleChange,
-						handleBlur,
-						handleSubmit,
-					} = props;
+			<img className="logo" src={Logo} alt="logo" />
+			<div className="wrapper">
+				<h1>Register</h1>
+				<Formik
+					initialValues={{ email: '', password: '', confirmPassword: '' }}
+					onSubmit={async (values, { setSubmitting, resetForm }) => {
+						try {
+							await handleSubmit(values);
+							history.push('/Login');
+						} catch (error) {
+							alert(error);
+							resetForm();
+							values.password = '';
+						}
+						//	setTimeout(() => {
+						//		console.log('Logging in', values);
+						//		setSubmitting(false);
+						//	}, 1000);
+					}}
+					validationSchema={Yup.object().shape({
+						email: Yup.string().email().required('Required'),
+						password: Yup.string()
+							.required('No password provided.')
+							.min(6, 'Password is too short - should be 6 chars minimum.')
+							.matches(/(?=.*[0-9])/, 'Password must contain a number.'),
+						confirmPassword: Yup.string()
+							.oneOf([ Yup.ref('password') ], 'Confirm Password must matched Password')
+							.required('Confirm Password is required')
+					})}
+				>
+					{(props) => {
+						const { values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit } = props;
 
-					return (
-						<form onSubmit={handleSubmit}>
-							<label htmlFor="email">Email</label>
-							<input
-								name="email"
-								type="text"
-								placeholder="Enter your email"
-								value={values.email}
-								onChange={handleChange}
-								onBlur={handleBlur}
-								className={
-									errors.email && touched.email && 'error'
-								}
-							/>
-							{errors.email && touched.email && (
-								<div className="input-feedback">
-									{errors.email}
-								</div>
-							)}
+						return (
+							<form onSubmit={handleSubmit}>
+								<label htmlFor="email">Email</label>
+								<input
+									name="email"
+									type="text"
+									placeholder="Enter your email"
+									value={values.email}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.email && touched.email && 'error'}
+								/>
+								{errors.email && touched.email && <div className="input-feedback">{errors.email}</div>}
 
-							<label htmlFor="email">Password</label>
-							<input
-								name="password"
-								type="password"
-								placeholder="Enter your password"
-								value={values.password}
-								onChange={handleChange('password')}
-								onBlur={handleBlur('password')}
-								className={
-									errors.password &&
-									touched.password &&
-									'error'
-								}
-							/>
-							{errors.password && touched.password && (
-								<div className="input-feedback">
-									{errors.password}
-								</div>
-							)}
-							<label htmlFor="confirmPassword">
-								Confirm Password
-							</label>
-							<input
-								name="password"
-								type="password"
-								placeholder="re-enter your password"
-								value={values.confirmPassword}
-								onChange={handleChange('confirmPassword')}
-								onBlur={handleBlur('confirmPassword')}
-								className={
-									errors.confirmPassword &&
-									touched.confirmPassword &&
-									'error'
-								}
-							/>
-							{errors.confirmPassword &&
+								<label htmlFor="email">Password</label>
+								<input
+									name="password"
+									type="password"
+									placeholder="Enter your password"
+									value={values.password}
+									onChange={handleChange('password')}
+									onBlur={handleBlur('password')}
+									className={errors.password && touched.password && 'error'}
+								/>
+								{errors.password &&
+								touched.password && <div className="input-feedback">{errors.password}</div>}
+								<label htmlFor="confirmPassword">Confirm Password</label>
+								<input
+									name="password"
+									type="password"
+									placeholder="re-enter your password"
+									value={values.confirmPassword}
+									onChange={handleChange('confirmPassword')}
+									onBlur={handleBlur('confirmPassword')}
+									className={errors.confirmPassword && touched.confirmPassword && 'error'}
+								/>
+								{errors.confirmPassword &&
 								touched.confirmPassword && (
-									<div className="input-feedback">
-										{errors.confirmPassword}
-									</div>
+									<div className="input-feedback">{errors.confirmPassword}</div>
 								)}
 
-							<button type="submit" disabled={isSubmitting}>
-								Register
-							</button>
-						</form>
-					);
-				}}
-			</Formik>
+								<button type="submit" disabled={isSubmitting}>
+									Register
+								</button>
+							</form>
+						);
+					}}
+				</Formik>
 			</div>
-			</div>
+		</div>
 	);
 };
 export default Register;
