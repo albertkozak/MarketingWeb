2239e24246e5d5165d33c8be5e036bed635598d1
Athena <athenakozak@gmail.com>
2020-04-26 13:32:32 -0700

    added button to home screen & reorganized files #25

--- atack-marketing/src/screens/Home.js
+++ atack-marketing/src/screens/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import EventList from '../components/EventList'
+import EventList from '../components/events/EventList'
 
 const Home = () => {
 	return (
 		<div className="container">
-			<h1>Home Page</h1>
+			{/* <h1>Home Page</h1> */}
+			<button
+				className="eventButton"
+			>Create Event
+			</button> 
 			<EventList />
 		</div>
 	);
