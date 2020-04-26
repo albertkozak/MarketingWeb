40beeecd1c8c833b4f6f1c4f9363405e5158fe83
Athena <athenakozak@gmail.com>
2020-04-25 20:13:51 -0700

    added eventlist to home screen

--- atack-marketing/src/screens/Home.js
+++ atack-marketing/src/screens/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import EventList from '../components/EventList'
 
 const Home = () => {
 	return (
 		<div className="container">
 			<h1>Home Page</h1>
+			<EventList />
 		</div>
 	);
 };
