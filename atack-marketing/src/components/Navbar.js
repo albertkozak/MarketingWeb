9dd8f49ba9744091c9794b8f0a9c38301d13c1da
Kasra <kniktash@gmail.com>
2020-04-26 14:32:04 -0700

    Implemented router-dom Link to the nav elements
    We should use Link so that the page doesnt refresh on each click and components get rendered within router states

--- atack-marketing/src/components/Navbar.js
+++ atack-marketing/src/components/Navbar.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import logo from '../assets/nav-logo.png';
+import React from "react";
+import logo from "../assets/nav-logo.png";
+import { Link } from "react-router-dom";
 
 const NavComponent = () => {
-	return (
-		<header className="header">
-			<a href="/" className="logo">
-				<img src={logo} alt="" />
-			</a>
-			<input className="menu-btn" type="checkbox" id="menu-btn" />
-			<label className="menu-icon" htmlFor="menu-btn">
-				<span className="navicon" />
-			</label>
-			<ul className="menu">
-				{/* <div className="nav-menu"> */}
-				<li>
-					<a href="/Home">Home</a>
-				</li>
-				<li>
-					<a href="/QRCode">QR Code</a>
-				</li>
-				<li>
-					<a href="/Roles">Roles</a>
-				</li>
-				<li>
-					<a href="/Export">Export</a>
-				</li>
-				{/* </div> */}
-				{/* <div className="nav-auth"> */}
-				<li>
-					<a href="/Login">Login</a>
-				</li>
-				<li>
-					<a href="/Register">Register</a>
-				</li>
-				{/* </div> */}
-			</ul>
-		</header>
-	);
+  return (
+    <header className="header">
+      <a href="/" className="logo">
+        <img src={logo} alt="" />
+      </a>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" htmlFor="menu-btn">
+        <span className="navicon" />
+      </label>
+      <ul className="menu">
+        {/* <div className="nav-menu"> */}
+        <li>
+          <Link to="/Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/QRCode">QR Code</Link>
+        </li>
+        <li>
+          <Link to="/Roles">Roles</Link>
+        </li>
+        <li>
+          <Link to="/Export">Export</Link>
+        </li>
+        {/* </div> */}
+        {/* <div className="nav-auth"> */}
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/Register">Register</Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default NavComponent;
