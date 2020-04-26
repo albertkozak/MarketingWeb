eeb8ed68301c92b7d069361c0a93428f8d8221de
Athena <athenakozak@gmail.com>
2020-04-25 20:13:42 -0700

    Added eventlist

--- atack-marketing/src/components/EventItem.js
+++ atack-marketing/src/components/EventItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const EventItem = ({props}) => {
+const EventItem = (props) => {
+    console.log(props)
     return (
         <div className="eventItem">
-            <h3 className="eventTitle">{props.event.evetName}</h3>
+            <h3 className="eventTitle">{props.event.eventName}</h3>
             <p className="eventStartDateTime">{props.event.eventStartDateTime}</p>
             {/* links for these */}
             <div className="edit-del-links">Edit / Delete</div>
