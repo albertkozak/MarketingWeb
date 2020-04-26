72b97f2cb8e5a1326fe97ca0703e6ce8942e9b66
Athena <athenakozak@gmail.com>
2020-04-26 15:52:55 -0700

    linked create button to add event

--- atack-marketing/src/screens/Home.js
+++ atack-marketing/src/screens/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import EventList from '../components/events/EventList'
+import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+	const history = useHistory();
 	return (
 		<div className="container">
 			{/* <h1>Home Page</h1> */}
 			<button
 				className="eventButton"
+				onClick={() => history.push('/addevent')}
 			>Create Event
 			</button> 
 			<EventList />
