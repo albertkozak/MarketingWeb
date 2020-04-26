ecc4f6399ff7f176421e0140b4b82df7597fcb08
Albert Kozak <albert.kozak@live.com>
2020-04-25 18:22:05 -0700

    Added footer + styled for light theme

---
+++ atack-marketing/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<div className="footer-container">
+				<p>© 2020, Powered By Atack Marketing.</p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
