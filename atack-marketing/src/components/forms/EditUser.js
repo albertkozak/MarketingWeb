a8fe5e7e8e21b7a765784c1a4a74fb6c9e408721
Athena <athenakozak@gmail.com>
2020-04-30 12:29:17 -0700

    incorporated switch function to edit user

--- atack-marketing/src/components/forms/EditUser.js
+++ atack-marketing/src/components/forms/EditUser.js
@@ -1,46 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Switch from './Switch'
 
 const EditUser = (props) => {
 	const user = props.location.state.user;
 	const [isAdmin, setIsAdmin] = useState(user.isAdmin)
 
-	const editUser = async (user) => {
-		user.preventDefault();
-		const { userEmail, isAdmin } = user.target.elements;
+	const handleToggle = () => {
+		setIsAdmin(!isAdmin)
+		console.log(isAdmin)
 
-		// Add POST Request here
-		alert(`POST-request: ${userEmail.value} ${isAdmin.value}`);
-	};
-
-	const clearForm = (user) => {
-		user.preventDefault();
-		setIsAdmin(user.isAdmin)
-	};
-
-	//useEffect(() => {}, [])
+		// Add Put request here
+	}
 
 	return (
 		<div className="container">
-			<h1 className="addUserEmail">Edit User Access</h1>
-			<Switch 
-				isOn={isAdmin}
-        		handleToggle={() => setIsAdmin(!isAdmin)} />
-			<div className="userForm">
-				<form onSubmit={editUser} id="add-user-form" className="addUserForm">
-					<input readOnly value={user.userEmail} name="userEmail" type="text" placeholder="Email" />
-					
-					{/* <input
-            		onChange={updateIsAdmin}name="isAdmin" type="checkbox" label="Admin Access" /> */}
-					<div className="buttons">
-						<button className="submit" variant="" type="submit">
-							Edit Access
-						</button>
-						<button className="cancel" onClick={clearForm}>
-							Cancel
-						</button>
-					</div>
-				</form>
+			<h1 className="editUserTitle">Edit User Access</h1>
+			<h3 className="adminLabel">Admininstrative Access:</h3>
+			<div className="userContainer">
+			<p className="userName">{user.userEmail}</p>
+
+			<div className='switchContainer'>
+            <label className="switch">
+			<input 
+				type="checkbox" 
+				value={isAdmin} 
+				onChange={handleToggle} />
+            <div className="slider"></div>
+            </label>
+        	</div>
 			</div>
 		</div>
 	);
