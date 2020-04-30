e33f98e8eaf0abca270d25d2fb67daef8f357090
Athena <athenakozak@gmail.com>
2020-04-30 12:40:21 -0700

    styled roles page and added function to show admin status

--- atack-marketing/src/components/users/UserItem.js
+++ atack-marketing/src/components/users/UserItem.js
@@ -3,11 +3,23 @@ import { Link, withRouter } from "react-router-dom";
 
 const UserItem = (props) => {
 	const user = props.user
+	console.log(user)
+	
+	const showAdmin = () => {
+		let access;
+		if (user.isAdmin) {
+			access = "Admin"
+		} else {
+			access = ""
+		}
+		return access;
+	}
+
 
 	return (
 		<div className="userItem">
 			<h3 className="userTitle">{user.userEmail}</h3>
-			<p className="isAdmin">{user.isAdmin}</p>
+	<p className="isAdmin">{showAdmin()}</p>
 			{/* links for these */}
 			<div className="edit-del-links">
 			<Link
