34b8fa9f44510568e89dce9a877fcfb6e6394ce7
Albert Kozak <albert.kozak@live.com>
2020-04-29 19:07:12 -0700

    Adjusted edit event

--- atack-marketing/src/components/forms/EditEvent.js
+++ atack-marketing/src/components/forms/EditEvent.js
@@ -6,42 +6,57 @@ const EditEvent = (props) => {
 	const event = props.location.state.event;
 	const [ eventName, setEventName ] = useState(event.eventName);
 	const [ eventStartDateTime, setEventStartDateTime ] = useState(event.eventStartDateTime);
+	// Need to adjust selector form - pending
 	const [ venueName, setVenueName ] = useState(event.venue.venueName);
+	const [ errorMessage, setErrorMessage ] = useState('');
 	// Add GET Request here for Venues & Event Organizers
-	const dummyDataVenues = [
-		{
-			venueId: 1,
-			venueName: 'Rogers Arena',
-			website: 'https://rogersarena.com/'
-		},
-		{
-			venueId: 2,
-			venueName: 'Vancouver Convention Centre',
-			website: 'https://www.vancouverconventioncentre.com/'
-		}
-	];
+	// const dummyDataVenues = [
+	// 	{
+	// 		venueId: 1,
+	// 		venueName: 'Rogers Arena',
+	// 		website: 'https://rogersarena.com/'
+	// 	},
+	// 	{
+	// 		venueId: 2,
+	// 		venueName: 'Vancouver Convention Centre',
+	// 		website: 'https://www.vancouverconventioncentre.com/'
+	// 	}
+	// ];
 
-	const dummyDataEOs = [
-		{
-			eventOrganizerId: 1,
-			userId: 7,
-			userEmail: 'eventies@events.com'
-		},
-		{
-			eventOrganizerId: 2,
-			userId: 12,
-			userEmail: 'abc@123.com'
-		},
-		{
-			eventOrganizerId: 5,
-			userId: 198,
-			userEmail: 'whatisricebowl@wut.com'
-		}
-	];
+	// const dummyDataEOs = [
+	// 	{
+	// 		eventOrganizerId: 1,
+	// 		userId: 7,
+	// 		userEmail: 'eventies@events.com'
+	// 	},
+	// 	{
+	// 		eventOrganizerId: 2,
+	// 		userId: 12,
+	// 		userEmail: 'abc@123.com'
+	// 	},
+	// 	{
+	// 		eventOrganizerId: 5,
+	// 		userId: 198,
+	// 		userEmail: 'whatisricebowl@wut.com'
+	// 	}
+	// ];
 
 	const editEvent = async (event) => {
 		event.preventDefault();
 		const { eventName, eventStartDateTime, venueName } = event.target.elements;
+
+		// Validation
+		if (eventName.value === '' || eventStartDateTime.value === '' || venueName.value === '') {
+			setErrorMessage('Please fill all required fields.');
+		} else if (
+			eventName === event.eventName &&
+			eventStartDateTime.value === event.eventStartDateTime &&
+			eventVenueName.value === event.venueName
+		) {
+			setErrorMessages('No changes have been made');
+		} else {
+			setErrorMessages('');
+		}
 
 		// Add POST Request here
 		alert(`POST-request: ${eventName.value} ${eventStartDateTime.value} ${venueName}`);
@@ -49,16 +64,38 @@ const EditEvent = (props) => {
 
 	const clearForm = (event) => {
 		event.preventDefault();
-		document.getElementById('add-event-form').reset();
+		setEventName(event.eventName);
+		setEventStartDateTime(event.eventStartDateTime);
+		setVenueName(event.venueName);
+		// document.getElementById('add-event-form').reset();
 	};
+
+	useEffect(() => {}, [ errorMessage ]);
 
 	return (
 		<div className="container">
-			<h1 className="addEventTitle">Add Event</h1>
+			<h1 className="addEventTitle">Edit Event</h1>
 			<div className="eventForm">
-				<form onSubmit={editEvent} id="add-event-form" className="addEventForm">
-					<input name="eventName" type="text" placeholder="Title" />
-					<input name="eventStartDateTime" type="date" placeholder="Start Date" />
+				<form onSubmit={editEvent} id="edit-event-form" className="addEventForm">
+					<p className="form-error">{errorMessage}</p>
+					<input
+						onChange={(event) => {
+							setEventName(event.target.value);
+						}}
+						value={eventName}
+						name="eventName"
+						type="text"
+						placeholder="Title"
+					/>
+					<input
+						onChange={(event) => {
+							setEventStartDateTime(event.target.value);
+						}}
+						value={eventStartDateTime}
+						name="eventStartDateTime"
+						type="date"
+						placeholder="Start Date"
+					/>
 					<div className="input-selector">
 						<VenueInputSelector data={dummyDataVenues} />
 					</div>
--- atack-marketing/src/components/forms/EditVendor.js
+++ atack-marketing/src/components/forms/EditVendor.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EditVendor = (props) => {
 	const vendor = props.location.state.vendor;
-	const [name, setName] = useState(vendor.name)
-	const [description, setDescription] = useState(vendor.description)
-	const [email, setEmail] = useState(vendor.email)
-	const [website, setWebsite] = useState(vendor.website)
-	const [errorMessage, setErrorMessage] = useState("");
+	const [ name, setName ] = useState(vendor.name);
+	const [ description, setDescription ] = useState(vendor.description);
+	const [ email, setEmail ] = useState(vendor.email);
+	const [ website, setWebsite ] = useState(vendor.website);
+	const [ errorMessage, setErrorMessage ] = useState('');
 
 	const editVendor = async (vendor) => {
 		vendor.preventDefault();
@@ -15,45 +15,75 @@ const EditVendor = (props) => {
 		alert(`PUT-request: ${name.value} ${description.value} ${email.value} ${website.value}`);
 
 		// Validation
-		if (name.value === "") {
-			setErrorMessage("Please fill all required fields");
+		if (name.value === '') {
+			setErrorMessage('Please fill all required fields');
 		} else if (
-			name.value === vendor.name && 
+			name.value === vendor.name &&
 			description.value === vendor.description &&
 			email.value === vendor.email &&
 			website.value === vendor.website
-			) 
-		{
-			setErrorMessage("No changes have been made")
+		) {
+			setErrorMessage('No changes have been made');
 		} else {
-			setErrorMessage('')
+			setErrorMessage('');
 		}
 
 		// PUT Request
-	}
+	};
 
 	const clearForm = (vendor) => {
 		vendor.preventDefault();
-		setName(vendor.name)
-		setDescription(vendor.description)
-		setEmail(vendor.email)
-		setWebsite(vendor.website)
+		setName(vendor.name);
+		setDescription(vendor.description);
+		setEmail(vendor.email);
+		setWebsite(vendor.website);
 		//document.getElementById('add-vendor-form').reset();
 	};
 
-	useEffect(() => {}, [errorMessage])
-
+	useEffect(() => {}, [ errorMessage ]);
 
 	return (
 		<div className="container">
 			<h1 className="addVendorName">Edit Vendor</h1>
 			<div className="vendorForm">
 				<form onSubmit={editVendor} id="add-vendor-form" className="addVendorForm">
-				<p className="form-error">{errorMessage}</p>
-					<input onChange={event => { setName(event.target.value)}} value={name}name="name" type="text" placeholder="Vendor" />
-					<input onChange={event => { setDescription(event.target.value)}} value={description} name="description" type="text" placeholder="Description" />
-					<input onChange={event => { setEmail(event.target.value)}} value={email} name="email" type="text" placeholder="Email" />
-					<input onChange={event => { setWebsite(event.target.value)}} value={website} name="website" type="text" placeholder="Website" />
+					<p className="form-error">{errorMessage}</p>
+					<input
+						onChange={(event) => {
+							setName(event.target.value);
+						}}
+						value={name}
+						name="name"
+						type="text"
+						placeholder="Vendor"
+					/>
+					<input
+						onChange={(event) => {
+							setDescription(event.target.value);
+						}}
+						value={description}
+						name="description"
+						type="text"
+						placeholder="Description"
+					/>
+					<input
+						onChange={(event) => {
+							setEmail(event.target.value);
+						}}
+						value={email}
+						name="email"
+						type="text"
+						placeholder="Email"
+					/>
+					<input
+						onChange={(event) => {
+							setWebsite(event.target.value);
+						}}
+						value={website}
+						name="website"
+						type="text"
+						placeholder="Website"
+					/>
 					<div className="buttons">
 						<button className="submit" variant="" type="submit">
 							Add Vendor
