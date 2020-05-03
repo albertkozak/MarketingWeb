91188bd66563690d139239d85d5f91d630c5e0b8
Tony Koo <TonyKoo@msn.com>
2020-05-03 23:21:25 +0000

    Event Add Refactor

--- atack-marketing/src/components/forms/EventOrganizerInputSelector.js
+++ atack-marketing/src/components/forms/EventOrganizerInputSelector.js
@@ -1,40 +1,19 @@
-import React, {useState} from 'react';
+import React from "react";
 import Select from "react-dropdown-select";
 
-const EventOrganizerInputSelector = (props) => {
-	const [eventOrganizers, setEventOrganizers] = useState([]);
-	const eos = props.data;
-
-	function isSetEventOrganizers(value) {
-		setEventOrganizers(value)
-		console.log(eventOrganizers)
-		//props.parentCallback(value)
-	}
-
-	const options = eos.map((eo) => ({
-		value: eo,
-		label: eo.email
-	}))
+export default function EventOrganizerInputSelector(props) {
+  const options = props.users.map(eo => ({
+    value: eo,
+    label: eo.email
+  }));
 
   return (
     <Select
-				multi
-				//create
-				placeholder="Add event organizer(s)"
-				// options={props.data.map(data => ({
-				// 	label: data.userEmail,
-				// 	value: data.userId
-				// }))}
-				options={options}
-				values={[]}
-				onChange={(value) =>
-					isSetEventOrganizers(value)
-					//console.log(`%c > onChange  `, value)
-				}
-			/>
-  )
-} 
-
-EventOrganizerInputSelector.propTypes = {}
-
-export default EventOrganizerInputSelector;
+      multi
+      //Create
+      placeholder="Add event organizer(s)"
+      options={options}
+      onChange={props.handleEOSelect}
+    />
+  );
+}
