e122d85d852c352880c35cfd03f5aea2bbf06f9c
Tony Koo <TonyKoo@msn.com>
2020-05-05 00:35:23 +0000

    Edit Vendor, Update Time on Event Info, Add Moment

--- atack-marketing/src/components/forms/EditEvent.js
+++ atack-marketing/src/components/forms/EditEvent.js
@@ -1,198 +1,191 @@
-import React, { useState, useEffect } from 'react';
-import VenueInputSelector from './VenueInputSelector';
-import EventOrganizerInputSelector from './EventOrganizerInputSelector';
-import firebase from '../../firebase'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import VenueInputSelector from "./VenueInputSelector";
+import EventOrganizerInputSelector from "./EventOrganizerInputSelector";
+import firebase from "../../firebase";
 
-const EditEvent = (props) => {
-	const event = props.location.state.currentEvent;
-	const eventId = event.eventId;
-	const [ eventName, setEventName ] = useState(event.eventName);
-	const [ eventStartDateTime, setEventStartDateTime ] = useState(event.eventStartDateTime);
-	const formDate = new Date(eventStartDateTime).toISOString().slice(0, 10);
-	// Need to adjust selector form - pending
-	const [ venueName, setVenueName ] = useState(event.venue.venueName);
-	const [ errorMessage, setErrorMessage ] = useState('');
-	const [fetchedVenues, setFetchedVenues] = useState([])
-	const [fetchedUsers, setFetchedUsers] = useState([])
-	const [selectedVenue, setSelectedVenue] = useState('')
-	const [selectedEventOrganizers, setSelectedEventOrgainizers] = useState([])
+import * as moment from "moment-timezone";
 
-	const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/"
+import { BASE_URL } from "../../Config";
 
-	// Add GET Request here for Venues & Event Organizers
-	const fetchVenues = () => {
-		firebase
-		  .auth()
-		  .currentUser.getIdTokenResult()
-		  .then((tokenResponse) => {
-			fetch(BASE_URL + "Venues", {
-			  method: "GET",
-			  headers: {
-				Accept: "application/json",
-				Authorization: `Bearer ${tokenResponse.token}`,
-			  },
-			})
-			  .then((response) => response.json())
-			  .then((responseData) => {
-				setFetchedVenues(responseData);
-				console.log(responseData)
-				console.log(fetchedVenues);
-			  });
-		  });
-	  };
+export default function AddEvent(props) {
+  const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState("");
 
-	  const fetchUsers = () => {
-		firebase
-		  .auth()
-		  .currentUser.getIdTokenResult()
-		  .then((tokenResponse) => {
-			fetch(BASE_URL + "User/userlist", {
-			  method: "GET",
-			  headers: {
-				Accept: "application/json",
-				Authorization: `Bearer ${tokenResponse.token}`,
-			  },
-			})
-			  .then((response) => response.json())
-			  .then((responseData) => {
-				setFetchedUsers(responseData);
-				console.log(fetchedUsers);
-			  });
-		  });
-	  };
-	
-	  useEffect(() => {
-		fetchVenues(); 
-		fetchUsers();
-	  });
+  const [fetchedVenues, setFetchedVenues] = useState([]);
+  const [selectedVenue, setSelectedVenue] = useState([]);
 
-	const editEvent = async (event) => {
-		event.preventDefault();
-		const { eventName, eventStartDateTime, venueId } = event.target.elements;
+  const [eventId, setEventId] = useState(0);
+  const [eventName, setEventName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [startTime, setstartTime] = useState("");
 
-		// Validation
-		if (eventName.value === '' || eventStartDateTime.value === '' || selectedVenue === '') {
-			setErrorMessage('Please fill all required fields.');
-		} else if (
-			eventName.value === event.eventName &&
-			eventStartDateTime.value === event.eventStartDateTime &&
-			selectedVenue === event.venue.venueId
-		) {
-			console.log(eventName.value)
-			console.log(event.eventName)
-			console.log(eventStartDateTime.value)
-			console.log(event.eventStartDateTime)
-			console.log(formDate)
-			console.log(selectedVenue)
-			//console.log(event.venue.venueId)
-			setErrorMessage('No changes have been made');
+  useEffect(() => {
+    //Check if we have incoming data, if not kick them out
+    if (props.location.state === undefined) {
+      history.push("/");
+    } else {
+      fetchVenues();
+      let currentEvent = props.location.state.currentEvent;
+      setEventId(currentEvent.eventId);
+      setEventName(currentEvent.eventName);
+      setStartDate(
+        moment
+          .utc(currentEvent.eventStartDateTime)
+          .local()
+          .format("yyyy-MM-DD")
+      );
+      setstartTime(
+        moment
+          .utc(currentEvent.eventStartDateTime)
+          .local()
+          .format("HH:mm")
+      );
+      setSelectedVenue([
+        {
+          value: currentEvent.venue.venueId,
+          label: `${currentEvent.venue.venueName}`
+        }
+      ]);
+    }
+  }, []);
 
-		} else {
-			console.log(eventName.value)
-			console.log(event.eventName)
-			console.log(eventStartDateTime.value)
-			console.log(event.eventStartDateTime)
-			console.log(formDate)
-			console.log(selectedVenue)
-			//console.log(event.venue.venueId)
-			setErrorMessage('');
+  // Add GET Request here for Venues & Event Organizers
 
-			let JWToken = await (
-				await firebase.auth().currentUser.getIdTokenResult()).token;
-			
-				if (JWToken !== null) {
-					const result = await fetch(BASE_URL + "Events/update/" + eventId, {
-						method: "PUT",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-							Authorization: `Bearer ${JWToken}`
-						},
-						body: JSON.stringify({
-							eventName: eventName.value,
-							eventStartDateTime: eventStartDateTime.value,
-							venueId: selectedVenue
-						})
-					});
-					if (result.status === 200) {
-						window.location.href = "/"
-					} else {
-						alert("Error: Something went wrong, please try again");
-			}
-			document.getElementById('edit-event-form').reset();
-					}
-				}
-		}
+  function fetchVenues() {
+    firebase
+      .auth()
+      .currentUser.getIdTokenResult()
+      .then(tokenResponse => {
+        fetch(BASE_URL + "Venues", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${tokenResponse.token}`
+          }
+        })
+          .then(response => response.json())
+          .then(responseData => {
+            setFetchedVenues(
+              responseData.map(venue => ({
+                value: venue.venueId,
+                label: venue.venueName
+              }))
+            );
+          });
+      });
+  }
 
-		const callbackFunction = (childData) => {
-			//let venueId = childData.value;
-			//setSelectedVenue(childData)
-			console.log(childData)
-			console.log(childData.value)
-			console.log(selectedVenue)
-			setSelectedVenue(1)
-		}
-	
-		const callbackFunctionEOs = (childData) => {
-			//let venueId = childData.value;
-			//setSelectedVenue(childData)
-			console.log(childData)
-			console.log(childData.value)
-			setSelectedEventOrgainizers(1)
-		}
+  async function updateEvent(event) {
+    event.preventDefault();
 
-	const clearForm = (event) => {
-		event.preventDefault();
-		setEventName(event.eventName);
-		setEventStartDateTime(event.eventStartDateTime);
-		setVenueName(event.venueName);
-		// document.getElementById('add-event-form').reset();
-	};
+    //Parse Date
+    var eventDateParsed = new Date(`${startDate}T${startTime}`);
+    var eventNameTrimmed = eventName.trim();
 
-	useEffect(() => {}, [ errorMessage ]);
+    //Validate Other Fields
+    if (
+      eventNameTrimmed.length === 0 ||
+      selectedVenue.length === 0 ||
+      isNaN(eventDateParsed.getTime())
+    ) {
+      setErrorMessage("Please fill all required fields");
+    } else {
+      setErrorMessage("");
 
-	return (
-		<div className="container">
-			<h1 className="addEventTitle">Edit Event</h1>
-			<div className="eventForm">
-				<form onSubmit={editEvent} id="edit-event-form" className="addEventForm">
-					<p className="form-error">{errorMessage}</p>
-					<input
-						onChange={(event) => {
-							setEventName(event.target.value);
-						}}
-						value={eventName}
-						name="eventName"
-						type="text"
-						placeholder="Title"
-					/>
-					<input
-						onChange={(event) => {
-							setEventStartDateTime(event.target.value);
-						}}
-						value={formDate}
-						name="eventStartDateTime"
-						type="date"
-						placeholder="Start Date"
-					/>
-					<div className="input-selector">
-						<VenueInputSelector data={fetchedVenues} parentCallback={callbackFunction} />
-					</div>
-					<div className="input-selector">
-						<EventOrganizerInputSelector data={fetchedUsers} parentCallback={callbackFunctionEOs} />
-					</div>
-					<div className="buttons">
-						<button className="submit" variant="" type="submit">
-							Edit Event
-						</button>
-						<button className="cancel" onClick={clearForm}>
-							Cancel
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
-};
+      let JWToken = await firebase.auth().currentUser.getIdTokenResult();
 
-export default EditEvent;
+      if (JWToken !== null) {
+        const result = await fetch(BASE_URL + `Events/update/${eventId}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JWToken.token}`
+          },
+          body: JSON.stringify({
+            eventName: eventNameTrimmed,
+            eventStartDateTime: eventDateParsed,
+            venueId: selectedVenue[0].value
+          })
+        });
+
+        if (result.status === 200) {
+          await fetch(BASE_URL + `Events/${eventId}`, {
+            METHOD: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${JWToken.token}`
+            }
+          })
+            .then(response => response.json())
+            .then(data => history.push("/event", { event: data }));
+        } else {
+          alert("Error: Something went wrong, please try again");
+        }
+      }
+    }
+  }
+
+  //Form Handlers
+  function cancelButton(event) {
+    event.preventDefault();
+    history.goBack();
+  }
+
+  function handleVenueSelect(selection) {
+    setSelectedVenue(selection);
+  }
+
+  return (
+    <div className="container">
+      <h1 className="addEventTitle">Edit Event</h1>
+      <div className="eventForm">
+        <form
+          onSubmit={updateEvent}
+          id="add-event-form"
+          className="addEventForm"
+        >
+          <p className="form-error">{errorMessage}</p>
+          <input
+            name="eventName"
+            type="text"
+            placeholder="Title"
+            value={eventName}
+            onChange={e => setEventName(e.target.value)}
+          />
+          <input
+            name="eventStartDate"
+            type="date"
+            placeholder="Start Date"
+            value={startDate}
+            onChange={e => setStartDate(e.target.value)}
+          />
+          <input
+            name="eventStartTime"
+            type="time"
+            placeholder="Start Time"
+            value={startTime}
+            onChange={e => setstartTime(e.target.value)}
+          />
+          <div className="input-selector">
+            <VenueInputSelector
+              options={fetchedVenues}
+              values={selectedVenue}
+              handleVenueSelect={handleVenueSelect}
+            />
+          </div>
+          <div className="buttons">
+            <button className="submit" variant="" type="submit">
+              Update
+            </button>
+            <button className="cancel" onClick={cancelButton}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
