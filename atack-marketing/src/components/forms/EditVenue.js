1418b33c16ce78c26481ebac29090f8d4a27e271
Athena <athenakozak@gmail.com>
2020-05-06 12:11:00 -0700

    Connected edit venue to api

--- atack-marketing/src/components/forms/EditVenue.js
+++ atack-marketing/src/components/forms/EditVenue.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import firebase from '../../firebase'
+import { useHistory } from "react-router-dom";
 
 const EditVenue = (props) => {
-	const venue = props.location.state.venue;
+	//const venue = props.location.state.venue;
 	// for passing to get back to previous selected event
 	//const eventId = props.location.state.id
-	const id = venue.venueId;
-    const [venueName, setVenueName] = useState(venue.venueName)
-    const [website, setWebsite] = useState(venue.website)
+	//const id = venue.venueId;
+	const [venueId, setVenueId] = useState(0)
+    const [venueName, setVenueName] = useState([])
+    const [website, setWebsite] = useState([])
 	const [errorMessage, setErrorMessage] = useState("")
-	const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Venues/update/"
+	const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/Venues/"
+	const history = useHistory();
+
+	useEffect(() => {
+		if (props.location.state === undefined) {
+			history.goBack()
+		} else {
+			let venue = props.location.state.venue;
+			setVenueId(venue.venueId)
+			setVenueName(venue.venueName)
+			setWebsite(venue.website)
+		}
+	}, 
+	[errorMessage])
 	
 	const editVenue = async (event) => {
 		event.preventDefault();
-        const { venueName, website } = event.target.elements;
+		//const { venueName, website } = event.target.elements;
+		
+		//Clean
+		var venueNameTrimmed = venueName.trim()
+		var websiteTrimmed = website.trim()
         
         // Validation
-        if (venueName.value === "" || website.value === "") {
+        if (venueNameTrimmed === "" || websiteTrimmed === "") {
             setErrorMessage("Please fill all required fields")
-        } else if (
-            venueName.value === venue.venueName &&
-            website.value === venue.website
-        ) {
-            setErrorMessage("No changes have been made")
         } else {
 			setErrorMessage("")
 			
@@ -30,7 +44,7 @@ const EditVenue = (props) => {
 				await firebase.auth().currentUser.getIdTokenResult()).token;
 
 				if(JWToken !== null) {
-					const result = await fetch (BASE_URL + id, {
+					const result = await fetch (BASE_URL + "update/" + venueId, {
 						method: "PUT",
 						headers: {
 							Accept: "application/json",
@@ -38,27 +52,35 @@ const EditVenue = (props) => {
 							Authorization: `Bearer ${JWToken}`
 						},
 						body: JSON.stringify({
-							venueName: venueName.value,
-							website: website.value
+							venueName: venueNameTrimmed,
+							website: websiteTrimmed
 						})
 					});
+					console.log(BASE_URL)
 					if (result.status === 200) {
-						window.location.href = "/"
+						await fetch(BASE_URL + venueId, {
+							METHOD: "GET",
+							headers: {
+							  Accept: "application/json",
+							  "Content-Type": "application/json",
+							  Authorization: `Bearer ${JWToken}`
+							}
+						  })
+							.then(response => response.json())
+							.then(data => history.push("/viewvenue", { venue: data }));
 					} else {
 						alert("Error: Something went wrong, please try again");
 			}
-			document.getElementById('edit-venue-form').reset();
 					}
 				}
 		}
 
 	const clearForm = (event) => {
 		event.preventDefault();
-        setVenueName(venue.venueName)
-        setWebsite(venue.website)
+		history.goBack();
+        // setVenueName(venue.venueName)
+        // setWebsite(venue.website)
     };
-    
-    useEffect(() => [errorMessage])
 
 	return (
 		<div className="container">
