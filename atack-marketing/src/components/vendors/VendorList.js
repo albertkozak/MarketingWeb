f4eccd521768435aeac8a72d31868bcc8001e071
Albert Kozak <albert.kozak@live.com>
2020-05-06 13:05:03 -0700

    Applied search filter to VendorList

--- atack-marketing/src/components/vendors/VendorList.js
+++ atack-marketing/src/components/vendors/VendorList.js
@@ -1,37 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import VendorItem from './VendorItem';
+import React, { useState, useEffect } from "react";
+import VendorItem from "./VendorItem";
 import firebase from "../../firebase";
+import SearchBar from "../SearchBar";
 
 const VendorList = (props) => {
-	const BASE_URL = "https://atackmarketingapi.azurewebsites.net/api/VendorManagement"
-	const [ fetchedVendors, setFetchedVendors] = useState([]);
+  const BASE_URL =
+    "https://atackmarketingapi.azurewebsites.net/api/VendorManagement";
+  const [fetchedVendors, setFetchedVendors] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchedVendors, setSearchedVendors] = useState("");
 
-	const fetchVendors = () => {
-		firebase
-		  .auth()
-		  .currentUser.getIdTokenResult()
-		  .then((tokenResponse) => {
-			fetch(BASE_URL, {
-			  method: "GET",
-			  headers: {
-				Accept: "application/json",
-				Authorization: `Bearer ${tokenResponse.token}`,
-			  },
-			})
-			  .then((response) => response.json())
-			  .then((responseData) => {
-				setFetchedVendors(responseData);
-				console.log(responseData)
-				console.log(fetchedVendors);
-			  });
-		  });
-	  };
+  const fetchVendors = () => {
+    firebase
+      .auth()
+      .currentUser.getIdTokenResult()
+      .then((tokenResponse) => {
+        fetch(BASE_URL, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${tokenResponse.token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            setFetchedVendors(responseData);
+            console.log(responseData);
+            console.log(fetchedVendors);
+          });
+        setSearchedVendors(
+          fetchedVendors.filter((vendor) => {
+            return vendor.name.toLowerCase().includes(search.toLowerCase());
+          })
+        );
+      });
+  };
 
-	  useEffect(() => {
-		fetchVendors();
-	  }, []);
+  useEffect(() => {
+    fetchVendors();
+  }, []);
 
-	return <div className="wrapper">{fetchedVendors.map((vendor) => <VendorItem key={vendor.id} vendor={vendor} />)}</div>;
+  let vendorData;
+  if (search.length === 0) {
+    vendorData = fetchedVendors;
+  } else {
+    vendorData = searchedVendors;
+  }
+
+  return (
+    <div className="wrapper">
+      <SearchBar
+        search={search}
+        onTermChange={(newSearch) => setSearch(newSearch)}
+        onTermSubmit={() => fetchVendors()}
+      />
+      {vendorData.map((vendor) => (
+        <VendorItem key={vendor.id} vendor={vendor} />
+      ))}
+    </div>
+  );
 };
 
 export default VendorList;
