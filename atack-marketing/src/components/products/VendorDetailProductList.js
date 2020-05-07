6d0b83dce4d5fc7b248f094bb9195dcf3f571c1b
Tony Koo <TonyKoo@msn.com>
2020-05-07 00:31:13 +0000

    Merge Product/QR/Reports Rough Code

--- atack-marketing/src/components/products/VendorDetailProductList.js
+++ atack-marketing/src/components/products/VendorDetailProductList.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import firebase from "../../firebase";
 import { useHistory } from "react-router-dom";
 import currency from "currency.js";
 import { faTimes, faUserPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BASE_URL } from "../../Config";
+import { QRCode } from "react-qr-svg";
 
 export default function VendorDetailProductList(props) {
   const eventId = props.location.state.eventId;
   const eventName = props.location.state.eventName;
   const eventVendorId = props.location.state.eventVendorId;
 
-  const DELETEPRODUCT_URL = `https://atackmarketingapi.azurewebsites.net/api`;
-
   const [fetchedData, setFetchedData] = useState([]);
   const [vendorDetails, setVendorDetails] = useState({});
 
   const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState(0.0);
+  const [productPrice, setProductPrice] = useState("");
+
+  const [updatingProdcut, setUpdatingProduct] = useState(null);
+  const inputProductName = useRef();
 
   function fetchData() {
+    setUpdatingProduct(null);
     firebase
       .auth()
       .currentUser.getIdTokenResult()
@@ -33,7 +36,7 @@ export default function VendorDetailProductList(props) {
         })
           .then(response => response.json())
           .then(responseData => {
-            setVendorDetails(responseData);
+            setVendorDetails(responseData.vendor);
             setFetchedData(responseData.vendor.products);
             console.log(responseData);
           });
@@ -45,8 +48,14 @@ export default function VendorDetailProductList(props) {
   }, []);
 
   async function createProduct(event) {
+    setUpdatingProduct(null);
     event.preventDefault();
     let JWToken = await firebase.auth().currentUser.getIdTokenResult();
+
+    if (productName.trim().length === 0) {
+      alert("Please Fill In The Product Name");
+      return;
+    }
 
     if (JWToken !== null) {
       const result = await fetch(
@@ -67,7 +76,47 @@ export default function VendorDetailProductList(props) {
       if (result.status === 201) {
         console.log("succcess");
         fetchData();
-        clearForm();
+        setProductName("");
+        setProductPrice("");
+        inputProductName.current.focus();
+      } else {
+        alert("Error: Something went wrong, please try again");
+      }
+    }
+  }
+
+  async function updateProduct(event) {
+    event.preventDefault();
+    let JWToken = await firebase.auth().currentUser.getIdTokenResult();
+
+    if (productName.trim().length === 0) {
+      alert("Please Fill In The Product Name");
+      return;
+    }
+
+    if (JWToken !== null) {
+      const result = await fetch(
+        BASE_URL +
+          `/EventVendor/${eventVendorId}/products/${updatingProdcut.productId}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JWToken.token}`
+          },
+          body: JSON.stringify({
+            productName: productName,
+            productPrice: currency(productPrice)
+          })
+        }
+      );
+      if (result.status === 200) {
+        console.log("succcess");
+        fetchData();
+        setProductName("");
+        setProductPrice("");
+        setUpdatingProduct(null);
       } else {
         alert("Error: Something went wrong, please try again");
       }
@@ -75,12 +124,12 @@ export default function VendorDetailProductList(props) {
   }
 
   async function removeProduct(productId) {
+    setUpdatingProduct(null);
     let JWToken = await firebase.auth().currentUser.getIdTokenResult();
 
     if (JWToken !== null) {
       const result = await fetch(
-        DELETEPRODUCT_URL +
-          `/EventVendor/${eventVendorId}/products/${productId}`,
+        BASE_URL + `/EventVendor/${eventVendorId}/products/${productId}`,
         {
           method: "DELETE",
           headers: {
@@ -98,86 +147,126 @@ export default function VendorDetailProductList(props) {
     }
   }
 
-  const clearForm = event => {
-    //event.preventDefault();
-    document.getElementById("add-product-form").reset();
-  };
-  const format = amount => {
+  //Utility Functions
+  function clearForm(event) {
+    event.preventDefault();
+    setProductName("");
+    setProductPrice("");
+  }
+
+  function updateProductButton(product) {
+    setUpdatingProduct(product);
+    setProductName(product.productName);
+    setProductPrice(product.productPrice);
+    inputProductName.current.focus();
+    window.scrollTo({
+      behavior: "smooth",
+      top: 0
+    });
+  }
+
+  function format(amount) {
     return Number(amount)
       .toFixed(2)
       .replace(/\d(?=(\d{3})+\.)/g, "$&,");
-  };
+  }
 
   return (
-    <>
-      <div className="container">
-        <h1 className="addProductTitle">Add Products</h1>
-        <div>
-          <form
-            onSubmit={createProduct}
-            id="add-product-form"
-            className="addProductForm"
-          >
-            <input
-              name="productName"
-              type="text"
-              placeholder="Product"
-              value={productName}
-              onChange={e => setProductName(e.target.value)}
-            />
-            <input
-              name="productPrice"
-              type="number"
-              step=".01"
-              min="0"
-              placeholder="Price"
-              value={productPrice}
-              onChange={e => setProductPrice(e.target.value)}
-            />
+    <div className="container">
+      <h1 className="addProductTitle">Manage Products</h1>
+      <div className="qrGenerator">
+        {Object.keys(vendorDetails).length > 0 && (
+          <QRCode
+            level="Q"
+            style={{ width: 256 }}
+            value={JSON.stringify({
+              eventId: eventId,
+              eventVendorId: eventVendorId,
+              vendorName: vendorDetails.vendorName
+            })}
+          />
+        )}
+      </div>
+      <div>
+        <form id="add-product-form" className="addProductForm">
+          <input
+            ref={inputProductName}
+            name="productName"
+            type="text"
+            placeholder="Product"
+            value={productName}
+            onChange={e => setProductName(e.target.value)}
+          />
+          <input
+            name="productPrice"
+            type="number"
+            step=".01"
+            min="0"
+            placeholder="Price"
+            value={productPrice}
+            onChange={e => setProductPrice(e.target.value)}
+          />
 
-            <div className="buttons">
-              <button className="submit" variant="" type="submit">
+          <div className="buttons">
+            {updatingProdcut !== null ? (
+              <button className="submit" variant="" onClick={updateProduct}>
+                Update
+              </button>
+            ) : (
+              <button className="submit" variant="" onClick={createProduct}>
                 Add Product
               </button>
-              <button className="cancel" onClick={clearForm}>
-                Cancel
-              </button>
-            </div>
-          </form>
-        </div>
+            )}
+
+            <button className="cancel" onClick={clearForm}>
+              Reset
+            </button>
+          </div>
+        </form>
       </div>
 
-      {Object.keys(vendorDetails).length > 0 && (
-        <div className="productList">
-          <h3> {vendorDetails.vendor.vendorName}</h3> <h3>{eventName}</h3>
-          <table className="productTable">
-            <thead>
-              <tr>
-                <th>Product Name</th>
-                <th>Product Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {fetchedData.map(product => (
-                <tr key={product.productId}>
-                  <td>{product.productName}</td>
-                  <td>{"$" + format(product.productPrice)}</td>
-                  <td>
-                    <FontAwesomeIcon
-                      className="delete"
-                      icon={faTimes}
-                      onClick={e => removeProduct(product.productId)}
-                    />
-                  </td>
-                  <td>
-                    <FontAwesomeIcon className="edit" icon={faEdit} />
-                  </td>
+      {Object.keys(vendorDetails).length > 0 &&
+        vendorDetails.products.length > 0 && (
+          <div className="productList">
+            <h3> {vendorDetails.vendorName}</h3> <h3>{eventName}</h3>
+            <table className="productTable">
+              <thead>
+                <tr>
+                  <th>Product Name</th>
+                  <th>Product Price</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-    </>
+              </thead>
+              <tbody>
+                {fetchedData.map(product => (
+                  <tr key={product.productId}>
+                    <td>{product.productName}</td>
+                    <td>{"$" + format(product.productPrice)}</td>
+                    <td>
+                      <FontAwesomeIcon
+                        className="delete"
+                        icon={faTimes}
+                        onClick={e =>
+                          window.confirm(
+                            `Are You Sure You Want To Delete: ${product.productName}`
+                          ) && removeProduct(product.productId)
+                        }
+                      />
+                    </td>
+                    <td>
+                      <FontAwesomeIcon
+                        className="edit"
+                        icon={faEdit}
+                        onClick={() => updateProductButton(product)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      {Object.keys(vendorDetails).length > 0 &&
+        vendorDetails.products.length === 0 && <p>No Products Yet!</p>}
+    </div>
   );
 }
